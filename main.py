27dd21a3878dcb2b41e2a5cfa8ca665d01fb6a4b
mzandvliet <martijn@ramjetanvil.com>
2019-06-04 17:57:21 +0200

    Added a straight port of Pomax's expansion for the full polynomial form of the curvature of a 2d cubic bezier

--- main.py
+++ main.py
@@ -58,13 +58,44 @@ def cool_stuff():
     mat = Matrix([[1,2],[2,2]])
     pprint(mat.eigenvals())
 
+# straight port from Mathematica snippet by Pomax
+# https://stackoverflow.com/questions/35901079/calculating-the-inflection-point-of-a-cubic-bezier-curve
+def bezier_inflection():
+    t, x1, x2, x3, x4, y1, y2, y3, y4 = symbols('t x1 x2 x3 x4 y1 y2 y3 y4')
+    a = 3 * (x2 - x1)
+    b = 3 * (x3 - x2)
+    c = 3 * (x4 - x3)
+    u = 2 * (b - a)
+    v = 2 * (c - b)
+
+    d = 3 * (y2 - y1)
+    e = 3 * (y3 - y2)
+    f = 3 * (y4 - y3)
+    w = 2 * (e - d)
+    z = 2 * (f - e)
+
+    bx1 = lambda t : a * (1 - t)**2 + 2 * b * (1 - t) * t + c * t**2
+    bx2 = lambda t : u * (1 - t) + v * t
+    by1 = lambda t : d * (1 - t)**2 + 2 * e * (1 - t) * t + f * t**2
+    by2 = lambda t : w * (1 - t) + z * t
+
+    curvature = bx1(t) * by2(t) - by1(t) * bx2(t)
+
+    result = expand(curvature)
+
+    pprint(result)
 
 def main():
     # literal_sqrt()
     # symbolic_sqrt()
     # simple_expr()
 
-    cool_stuff()
+    # cool_stuff()
+
+    # f = lambda x : x**2
+    # print("4^2: " + str(f(4)))
+
+    bezier_inflection()
     
 if __name__ == "__main__":
     main()
