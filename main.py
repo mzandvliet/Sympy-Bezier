cc3dffb6949a8fb9b880a44c7d5e7a901df77a10
mzandvliet <martijn@ramjetanvil.com>
2019-06-05 15:34:15 +0200

    Refactored old curvature equaltions creator with a new one that uses the bezier generators. Was wondering if I did it correctly, but of course you can quickly produce a proof to verify that you did! :D

--- main.py
+++ main.py
@@ -31,18 +31,15 @@ def to_latex_docstring(expr):
     return doc_srt
 
 
-def bezier_bases(n):
+def bezier_bases(n, param):
     bases = []
     for i in range(0, n+1):
-        bases.append(bernstein_basis(n, i))
+        bases.append(bernstein_basis(n, i, param))
 
     return bases
 
-def bernstein_basis(n, i):
-    symbs = symbols('t')
-    t = symbs
-
-    basis = binomial(n, i) * t**i * (1 - t)**(n-i)
+def bernstein_basis(n, i, param):
+    basis = binomial(n, i) * param**i * (1 - param)**(n-i)
 
     return basis
 
@@ -74,6 +71,50 @@ def bezier_curvature():
 
     return (symbs, result)
 
+def bezier_curvature_new():
+    symbs = symbols('t x1 x2 x3 x4 y1 y2 y3 y4')
+    t, x1, x2, x3, x4, y1, y2, y3, y4 = symbs
+    a = 3 * (x2 - x1)
+    b = 3 * (x3 - x2)
+    c = 3 * (x4 - x3)
+    u = 2 * (b - a)
+    v = 2 * (c - b)
+
+    d = 3 * (y2 - y1)
+    e = 3 * (y3 - y2)
+    f = 3 * (y4 - y3)
+    w = 2 * (e - d)
+    z = 2 * (f - e)
+
+    
+    bases_2nd = bezier_bases(2, t)
+    bases_3rd = bezier_bases(1, t)
+
+    points_x_2nd = (a, b, c)
+    points_x_3rd = (u, v)
+    points_y_2nd = (d, e, f)
+    points_y_3rd = (w, z)
+
+    bx1 = produce_bezier(points_x_2nd, bases_2nd)
+    bx2 = produce_bezier(points_x_3rd, bases_3rd)
+    by1 = produce_bezier(points_y_2nd, bases_2nd)
+    by2 = produce_bezier(points_y_3rd, bases_3rd)
+
+    curvature = bx1(t) * by2(t) - by1(t) * bx2(t)
+
+    result = expand(curvature)
+
+    return (symbs, result)
+
+
+def produce_bezier(points, bases):
+    if len(points) != len(bases):
+        raise Exception("Number of points %i should be equal to number of bases %i"%(len(points), len(bases)))
+
+    terms = [p * b for p, b in zip(points, bases)]
+    bezier = reduce((lambda x, y: x + y), terms)
+    func = lambda param: bezier
+    return func
 
 def simplify_curvature(symbs, expr):
     t, x1, x2, x3, x4, y1, y2, y3, y4 = symbs
@@ -116,8 +157,14 @@ def geometry():
     print(v)
 
 def main():
-    # symbs, expr = bezier_curvature();
-    # symbs, expr = simplify_curvature(symbs, expr)
+    symbs, expr = bezier_curvature_new();
+    symbs, expr = simplify_curvature(symbs, expr)
+
+    symbs2, expr2 = bezier_curvature()
+    symbs2, expr2 = simplify_curvature(symbs2, expr2)
+
+    are_equal = simplify(expr - expr2) == 0
+    pprint("Old and new equal? " + str(are_equal))
 
     # pprint(expr)
     # print("\nNumber of terms: " + str(len(expr.args)))
@@ -134,17 +181,16 @@ def main():
     # for b in bases:
     #     pprint(b)
 
-    a, b, c = symbols('a b c')
-    points = (a, b, c)
-    bases = bezier_bases(2)
-    # bezier_terms = [(lambda p, b: p * b)(a, b) for p, b in zip(points, bases)]
-    bezier_terms = [p * b for p, b in zip(points, bases)]
+    # a, b, c = symbols('a b c')
+    # points = (a, b, c)
+    # bases = bezier_bases(2)
+    # bezier_terms = [p * b for p, b in zip(points, bases)]
     
-    for t in bezier_terms:
-        pprint(t)
+    # for t in bezier_terms:
+    #     pprint(t)
 
-    bezier = reduce((lambda x, y: x + y), bezier_terms)
-    pprint(bezier)
+    # bezier = reduce((lambda x, y: x + y), bezier_terms)
+    # pprint(bezier)
     
 
     
