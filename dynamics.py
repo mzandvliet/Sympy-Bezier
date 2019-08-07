2fc79c56d077e16750c976e001bbc756f09de879
m_zandvliet <martijn@ramjetanvil.com>
2019-08-07 14:42:44 +0200

    Trying velocity for the end points, which turns out to bug out. Added lots of notes on what to do next.

--- dynamics.py
+++ dynamics.py
@@ -26,6 +26,10 @@ def intersect_quadratic_with_line():
     print_code(common, exprs)
 
 def quartic_bezier_wave_equation():
+    '''
+    Like the below 1-d version, but now with full
+    motion in the plane
+    '''
     # q = curve parameter
     # t = time
     # c = wave equation constant
@@ -63,6 +67,29 @@ def quartic_bezier_wave_equation():
     pprint(dp3_dt)
 
 def quartic_bezier_wave_equation_1d():
+    '''
+    Todo:
+    - Prove that for each control point you can chose a specific Qi,
+    such that the calculated velocity for Pi represents the change
+    with respect to the entire wave section.
+
+    See Readability notes for details, but one way to try it is through
+    defining the gradient as the integral of gradients along all Q, and
+    seeing whether something neat pops out.
+
+    The hunch would be that terms left and right of such a chosen Q
+    would cancel each other out.
+
+    - If that proof pans out, derive a formula that expresses such a Q_i
+    for any P_i
+
+    Factoid: End points P_1 and P_5 will have Q_1=0 and Q_5=1, so we
+    conclude that at least for those points,
+
+    Another hunch is that if a representative Q_i exists, it would probably
+    be the point Q on the curve that is **most affected** by change in P_i
+    '''
+
     # q = curve parameter
     # t = time
     # c = wave equation constant
@@ -94,11 +121,25 @@ def quartic_bezier_wave_equation_1d():
     # using the chain rule:
     # dp3_dt = dp/dp3 * dp_dt
 
+    dp_dp1 = diff(p[1], p1[1])
+    dp_dp2 = diff(p[1], p2[1])
     dp_dp3 = diff(p[1], p3[1])
+    dp_dp4 = diff(p[1], p4[1])
+    dp_dp5 = diff(p[1], p5[1])
+    dp1_dt = dp_dp1 * dp_dt
+    dp2_dt = dp_dp2 * dp_dt
     dp3_dt = dp_dp3 * dp_dt
-    # pprint(dp3_dt)
+    dp4_dt = dp_dp4 * dp_dt
+    dp5_dt = dp_dp5 * dp_dt
 
-    common, exprs = cse(dp3_dt, numbered_symbols('a'))
+    q1, q2, q3, q4, q5 = symbols('q1 q2 q3 q4 q5')
+    dp1_dt = dp1_dt.subs(q, q1)
+    dp2_dt = dp2_dt.subs(q, q2)
+    dp3_dt = dp3_dt.subs(q, q3)
+    dp4_dt = dp4_dt.subs(q, q4)
+    dp5_dt = dp5_dt.subs(q, q5)
+
+    common, exprs = cse((dp1_dt, dp2_dt, dp3_dt, dp4_dt, dp5_dt), numbered_symbols('a'))
     print_code(common, exprs)
 
 def main():
